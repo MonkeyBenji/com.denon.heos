fbf9684e93e359144be7f1613a574e133497a577
Emile Nijssen <emile@athom.nl>
2019-04-24 15:45:15 +0200

    v2.1.0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Denon Heos
 
 This app adds support for Denon Heos Wi-Fi speakers.
+
+Control your Denon Heos speaker by changing the volume, starting AUX-In with a Flow and other functionality.
--- drivers/heos/driver.js
+++ drivers/heos/driver.js
@@ -5,4 +5,14 @@ const DenonHeosHomeyDriver = require('../../lib/DenonHeosHomeyDriver');
 
 module.exports = class extends DenonHeosHomeyDriver {
 	
+	onInit(...props) {
+  	super.onInit(...props);
+  	
+  	new Homey.FlowCardAction('play_aux_in')
+  	  .register()
+  	  .registerRunListener(({ device }) => {
+    	  return device.playAUXIn();
+  	  });
+	}
+	
 }
